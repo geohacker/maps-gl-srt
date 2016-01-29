300a6daf51e1ee80dba703be28b760c627e9401e
Sajjad Anwar <me@sajjad.in>
2016-01-29 14:39:39 +0530

    new video.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
     window.map = new mapboxgl.Map({
         container: 'map', // container id
         style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
-        center: [77.58518368005753, 12.963926254055531],
-        zoom: 16 // starting zoom
+        center: [77.61776, 12.99873],
+        zoom: 18 // starting zoom
     });
 
-    mapSRT(map, ['https://video500.pad.ma/WX/240p1.webm'], 'test.srt', {
+    mapSRT(map, ['https://video500.pad.ma/HWD/240p1.webm'], 'test.srt', {
         'muted': true
     });
 
